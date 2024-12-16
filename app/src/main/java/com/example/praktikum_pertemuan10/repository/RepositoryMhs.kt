496baa2ae9e2348ec7e2e0ec50f7a4a8361ac6ab
Wahyu <firmansyahw503@gmail.com>
2024-12-16 15:12:48 +0700

    Commit 6 (Metode untuk mengambil fungsi di MahasiswaDao)

--- app/src/main/java/com/example/praktikum_pertemuan10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum_pertemuan10/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.praktikum_pertemuan10.repository
 
 import com.example.praktikum_pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
 }
