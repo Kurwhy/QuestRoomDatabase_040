1d83357577a6c88f85223d45b98afa8c7b4bb4c3
Wahyu <firmansyahw503@gmail.com>
2024-12-16 15:08:52 +0700

    Commit 3 (Metode untuk mengubah data)

--- app/src/main/java/com/example/praktikum_pertemuan10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/praktikum_pertemuan10/data/dao/MahasiswaDao.kt
@@ -3,6 +3,7 @@ package com.example.praktikum_pertemuan10.data.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Update
 import com.example.praktikum_pertemuan10.data.entity.Mahasiswa
 
 @Dao
@@ -11,5 +12,6 @@ interface MahasiswaDao {
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
     @Delete
     suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
-
+    @Update
+    suspend fun updateMahasiswa (mahasiswa: Mahasiswa)
 }
