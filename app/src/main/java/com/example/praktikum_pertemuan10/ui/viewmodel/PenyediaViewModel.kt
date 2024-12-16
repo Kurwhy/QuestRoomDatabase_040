eeddcf2b58c7f7c50eec07cee8b09088a7dbcde9
Wahyu <firmansyahw503@gmail.com>
2024-12-16 15:41:27 +0700

    Commit 12 (Melengkapi daftar view model)

--- app/src/main/java/com/example/praktikum_pertemuan10/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum_pertemuan10/ui/viewmodel/PenyediaViewModel.kt
@@ -3,6 +3,7 @@ package com.example.praktikum_pertemuan10.ui.viewmodel
 import android.text.Editable.Factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -12,6 +13,25 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
