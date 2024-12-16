3dca0e7801bf551044d19316b3e985105f2edd6d
Wahyu <firmansyahw503@gmail.com>
2024-12-16 15:07:48 +0700

    Commit 2 (Metode untuk menghapus data)

--- app/src/main/java/com/example/praktikum_pertemuan10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/praktikum_pertemuan10/data/dao/MahasiswaDao.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum_pertemuan10.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import com.example.praktikum_pertemuan10.data.entity.Mahasiswa
 
@@ -8,4 +9,7 @@ import com.example.praktikum_pertemuan10.data.entity.Mahasiswa
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    @Delete
+    suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
+
 }
