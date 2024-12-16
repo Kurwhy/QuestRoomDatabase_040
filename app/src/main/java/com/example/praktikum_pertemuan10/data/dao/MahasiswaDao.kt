0bcdbb0bb584b30dd6217c0f2387abeee10aa265
Wahyu <firmansyahw503@gmail.com>
2024-12-16 15:09:40 +0700

    Commit 4 (Metode untuk mengambil semua data mahasiswa)

--- app/src/main/java/com/example/praktikum_pertemuan10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/praktikum_pertemuan10/data/dao/MahasiswaDao.kt
@@ -3,8 +3,10 @@ package com.example.praktikum_pertemuan10.data.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.example.praktikum_pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
@@ -14,4 +16,6 @@ interface MahasiswaDao {
     suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
     @Update
     suspend fun updateMahasiswa (mahasiswa: Mahasiswa)
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 }
